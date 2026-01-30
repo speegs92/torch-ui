5f883d21e33eb8e93337ba2d16662bef27c98317
speegs92 <christian@levesque.dev>
2026-01-30 16:44:18 -0600

    added <CarouselImage> component to represent an image for use inside carousels (with .d-block and .w-100 applied)

---
+++ src/TorchUI.Bootstrap/Components/Carousels/CarouselImage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a full-width, block-rendered <c>&lt;img&gt;</c> for use inside Bootstrap carousels
+/// </summary>
+public class CarouselImage : TorchComponentBase
+{
+	/// <summary>
+	/// The image's <c>src</c> attribute value. Required
+	/// </summary>
+	[Parameter]
+	public required string Src { get; set; }
+
+	/// <summary>
+	/// The image's <c>alt</c> attribute value. Required
+	/// </summary>
+	[Parameter]
+	public required string Alt { get; set; }
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		Tag = "img";
+		CssBuilder.AddClass("d-block w-100");
+		UserAttributes["src"] = Src;
+		UserAttributes["alt"] = Alt;
+	}
+}
