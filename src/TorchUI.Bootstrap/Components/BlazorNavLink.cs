1328b2c5abf035d639cd738c68d92fc59dae5a2e
speegs92 <christian@levesque.dev>
2026-02-01 12:06:54 -0600

    added <BlazorNavLink> component to restore the built-in <NavLink> component

---
+++ src/TorchUI.Bootstrap/Components/BlazorNavLink.cs
@@ -0,0 +1,6 @@
+﻿namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Restores the built-in <see cref="Microsoft.AspNetCore.Components.Routing.NavLink"/> component
+/// </summary>
+public class BlazorNavLink : Microsoft.AspNetCore.Components.Routing.NavLink;
