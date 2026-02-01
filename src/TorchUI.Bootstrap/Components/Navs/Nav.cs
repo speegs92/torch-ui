707e79032f69970d7a059f502da7b1a31b942f69
speegs92 <christian@levesque.dev>
2026-02-01 12:06:31 -0600

    added Blazor nav

---
+++ src/TorchUI.Bootstrap/Components/Navs/Nav.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.nav</c>
+/// </summary>
+public class Nav : TorchComponentBase
+{
+	/// <summary>
+	/// Whether the nav's content should unevenly fill the available space
+	/// </summary>
+	[Parameter]
+	public bool Fill { get; set; }
+
+	/// <summary>
+	/// Whether the nav's content should evenly fill the available space
+	/// </summary>
+	[Parameter]
+	public bool Justified { get; set; }
+
+	/// <summary>
+	/// Whether the nav should be rendered in pill style
+	/// </summary>
+	[Parameter]
+	public bool Pills { get; set; }
+
+	/// <summary>
+	/// Whether the nav should be rendered in tab style
+	/// </summary>
+	[Parameter]
+	public bool Tabs { get; set; }
+
+	/// <summary>
+	/// Whether the nav should be rendered in underline style
+	/// </summary>
+	[Parameter]
+	public bool Underline { get; set; }
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		Tag ??= "ul";
+		CssBuilder.AddClass("nav");
+
+		if (Fill)
+		{
+			CssBuilder.AddClass("nav-fill");
+		}
+		else if (Justified)
+		{
+			CssBuilder.AddClass("nav-justified");
+		}
+
+		if (Pills)
+		{
+			CssBuilder.AddClass("nav-pills");
+		}
+		else if (Tabs)
+		{
+			CssBuilder.AddClass("nav-tabs");
+		}
+		else if (Underline)
+		{
+			CssBuilder.AddClass("nav-underline");
+		}
+	}
+}
---
+++ src/TorchUI.Bootstrap/Components/Navs/NavItem.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.nav-item</c>
+/// </summary>
+public partial class NavItem
+{
+	/// <summary>
+	/// The destination of the nav item
+	/// </summary>
+	/// <remarks>
+	/// If this is <see langword="null"/>, the <c>NavItem</c> is rendered without an inner <c>&lt;a&gt;</c>. If you wish to customize the <c>&lt;a&gt;</c> markup or styling, omit this parameter and supply your own.
+	/// </remarks>
+	[Parameter]
+	public string? Href { get; set; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		Tag ??= "li";
+		CssBuilder.AddClass("nav-item");
+	}
+}
