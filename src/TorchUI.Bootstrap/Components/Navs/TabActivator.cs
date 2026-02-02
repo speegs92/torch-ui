a016cc8289afd4ed0aeba98d62e42580adbca485
speegs92 <christian@levesque.dev>
2026-02-01 18:43:50 -0600

    added <TabList> and associated components

---
+++ src/TorchUI.Bootstrap/Components/Navs/TabActivator.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// An activator button for a <see cref="TabPane"/>
+/// </summary>
+public class TabActivator : TorchComponentBase
+{
+	/// <summary>
+	/// The activator's HTML ID
+	/// </summary>
+	[Parameter]
+	[EditorRequired]
+	public string Id { get; set; }
+
+	/// <summary>
+	/// The HTML ID of the tab pane the tab activator activates
+	/// </summary>
+	[Parameter]
+	[EditorRequired]
+	public string TabPaneId { get; set; }
+
+	/// <summary>
+	/// Whether the tab activator's tab is active
+	/// </summary>
+	[Parameter]
+	public bool Active { get; set; }
+
+	/// <summary>
+	/// Whether the tab activator is disabled
+	/// </summary>
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		Tag = "button";
+
+		CssBuilder.AddClass("nav-link");
+
+		UserAttributes["type"] = "button";
+		UserAttributes["role"] = "tab";
+		UserAttributes["data-bs-toggle"] = "tab";
+		UserAttributes["data-bs-target"] = $"#{TabPaneId}";
+		UserAttributes["aria-controls"] = TabPaneId;
+
+		if (Active)
+		{
+			CssBuilder.AddClass("active");
+			UserAttributes["aria-selected"] = "true";
+		}
+
+		if (Disabled)
+		{
+			CssBuilder.AddClass("disabled");
+			UserAttributes["disabled"] = "disabled";
+		}
+	}
+
+	/// <summary>
+	/// The parameters to provide to the <see cref="TabActivator"/> component
+	/// </summary>
+	public class TabActivatorParameters
+	{
+		/// <summary>
+		/// The activator's HTML ID
+		/// </summary>
+		public required string ActivatorId { get; set; }
+
+		/// <summary>
+		/// The HTML ID of the tab pane the tab activator activates
+		/// </summary>
+		public required string TabPaneId { get; set; }
+
+		/// <summary>
+		/// Whether the tab activator's tab is active
+		/// </summary>
+		public bool Active { get; set; }
+
+		/// <summary>
+		/// Whether the tab activator is disabled
+		/// </summary>
+		public bool Disabled { get; set; }
+	}
+}
---
+++ src/TorchUI.Bootstrap/Components/Navs/TabPane.razor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.tab-pane</c>
+/// </summary>
+public partial class TabPane : IDisposable
+{
+	private string _tabPaneId = string.Empty;
+	private readonly string _fallbackId = Guid
+		.NewGuid()
+		.ToString();
+
+	/// <summary>
+	/// The activator button's ID
+	/// </summary>
+	[Parameter]
+	public string ActivatorId { get; set; } = Guid
+		.NewGuid()
+		.ToString();
+
+	/// <summary>
+	/// The tab pane's activator text
+	/// </summary>
+	[Parameter]
+	public string? Title { get; set; }
+
+	/// <summary>
+	/// The tab's activator button
+	/// </summary>
+	[Parameter]
+	public RenderFragment<TabActivator.TabActivatorParameters>? Activator { get; set; }
+
+	/// <summary>
+	/// Whether the tab pane should be rendered as active
+	/// </summary>
+	[Parameter]
+	public bool Active { get; set; }
+
+	/// <summary>
+	/// Whether the tab pane should fade on transition
+	/// </summary>
+	[Parameter]
+	public bool Fade { get; set; }
+
+	/// <summary>
+	/// Whether the tab pane should be disabled
+	/// </summary>
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	/// <summary>
+	/// The containing tab list
+	/// </summary>
+	[CascadingParameter]
+	public TabList? Parent { get; set; }
+
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		Parent?.Register(this);
+	}
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		CssBuilder
+			.AddClass("tab-pane")
+			.AddClass("active", Active)
+			.AddClass("fade", Fade);
+
+		_tabPaneId = GetOrSetAttribute("id", _fallbackId);
+	}
+
+	/// <inheritdoc />
+	public void Dispose()
+	{
+		Parent?.Unregister(this);
+		GC.SuppressFinalize(this);
+	}
+}
