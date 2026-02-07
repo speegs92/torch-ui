dce4cce4e5ad4791623001e9b454cf82978cd173
speegs92 <christian@levesque.dev>
2026-02-07 13:38:56 -0600

    moved <Nav> functionality to new <NavBase> component, which contains most of the render logic for <Nav> except adding the .nav class

--- src/TorchUI.Bootstrap/Components/Navs/Nav.cs
+++ src/TorchUI.Bootstrap/Components/Navs/Nav.cs
@@ -1,69 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
-
-// ReSharper disable once CheckNamespace
+﻿// ReSharper disable once CheckNamespace
 namespace TorchUI.Bootstrap.Components;
 
 /// <summary>
 /// Represents a Bootstrap <c>.nav</c>
 /// </summary>
-public class Nav : TorchComponentBase
+public class Nav : NavBase
 {
-	/// <summary>
-	/// Whether the nav's content should unevenly fill the available space
-	/// </summary>
-	[Parameter]
-	public bool Fill { get; set; }
-
-	/// <summary>
-	/// Whether the nav's content should evenly fill the available space
-	/// </summary>
-	[Parameter]
-	public bool Justified { get; set; }
-
-	/// <summary>
-	/// Whether the nav should be rendered in pill style
-	/// </summary>
-	[Parameter]
-	public bool Pills { get; set; }
-
-	/// <summary>
-	/// Whether the nav should be rendered in tab style
-	/// </summary>
-	[Parameter]
-	public bool Tabs { get; set; }
-
-	/// <summary>
-	/// Whether the nav should be rendered in underline style
-	/// </summary>
-	[Parameter]
-	public bool Underline { get; set; }
-
 	/// <inheritdoc />
 	protected override void SetupAttributes()
 	{
-		Tag ??= "ul";
 		CssBuilder.AddClass("nav");
-
-		if (Fill)
-		{
-			CssBuilder.AddClass("nav-fill");
-		}
-		else if (Justified)
-		{
-			CssBuilder.AddClass("nav-justified");
-		}
-
-		if (Pills)
-		{
-			CssBuilder.AddClass("nav-pills");
-		}
-		else if (Tabs)
-		{
-			CssBuilder.AddClass("nav-tabs");
-		}
-		else if (Underline)
-		{
-			CssBuilder.AddClass("nav-underline");
-		}
+		base.SetupAttributes();
 	}
 }
---
+++ src/TorchUI.Bootstrap/Components/Navs/NavBase.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// A base nav with shared functionality
+/// </summary>
+public abstract class NavBase : TorchComponentBase
+{
+	/// <summary>
+	/// Whether the nav's content should unevenly fill the available space
+	/// </summary>
+	[Parameter]
+	public bool Fill { get; set; }
+
+	/// <summary>
+	/// Whether the nav's content should evenly fill the available space
+	/// </summary>
+	[Parameter]
+	public bool Justified { get; set; }
+
+	/// <summary>
+	/// Whether the nav should be rendered in pill style
+	/// </summary>
+	[Parameter]
+	public bool Pills { get; set; }
+
+	/// <summary>
+	/// Whether the nav should be rendered in tab style
+	/// </summary>
+	[Parameter]
+	public bool Tabs { get; set; }
+
+	/// <summary>
+	/// Whether the nav should be rendered in underline style
+	/// </summary>
+	[Parameter]
+	public bool Underline { get; set; }
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		Tag ??= "ul";
+
+		if (Fill)
+		{
+			CssBuilder.AddClass("nav-fill");
+		}
+		else if (Justified)
+		{
+			CssBuilder.AddClass("nav-justified");
+		}
+
+		if (Pills)
+		{
+			CssBuilder.AddClass("nav-pills");
+		}
+		else if (Tabs)
+		{
+			CssBuilder.AddClass("nav-tabs");
+		}
+		else if (Underline)
+		{
+			CssBuilder.AddClass("nav-underline");
+		}
+	}
+}
