c058254a90429cd5d56236c2571abae2b7400091
speegs92 <christian@levesque.dev>
2026-02-14 14:09:49 -0600

    added Direction enum to represent up, down, start, and end directions

---
+++ src/TorchUI.Bootstrap/Direction.cs
@@ -0,0 +1,27 @@
+﻿namespace TorchUI.Bootstrap;
+
+/// <summary>
+/// Represents various directions that are available within components
+/// </summary>
+public enum Direction
+{
+	/// <summary>
+	/// The up direction
+	/// </summary>
+	Up,
+
+	/// <summary>
+	/// The down direction
+	/// </summary>
+	Down,
+
+	/// <summary>
+	/// The start direction
+	/// </summary>
+	Start,
+
+	/// <summary>
+	/// The end direction
+	/// </summary>
+	End
+}
