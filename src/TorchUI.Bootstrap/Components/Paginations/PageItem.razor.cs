3484eef4418bce50b370314ef4a257372ed87709
speegs92 <christian@levesque.dev>
2026-02-15 11:59:48 -0600

    added <Pagination> and <PaginationItem> components

---
+++ src/TorchUI.Bootstrap/Components/Paginations/PageItem.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.page-item</c>
+/// </summary>
+public partial class PageItem
+{
+	/// <summary>
+	/// The <c>href</c> attribute of the pagination link
+	/// </summary>
+	[Parameter]
+	[EditorRequired]
+	public string Href { get; set; }
+
+	/// <summary>
+	/// The <c>aria-label</c> attribute of the pagination link
+	/// </summary>
+	[Parameter]
+	public string? Label { get; set; }
+
+	/// <summary>
+	/// Whether the page item is active
+	/// </summary>
+	[Parameter]
+	public bool Active { get; set; }
+
+	/// <summary>
+	/// Whether the page item is disabled
+	/// </summary>
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		CssBuilder
+			.AddClass("page-item")
+			.AddClass("active", Active)
+			.AddClass("disabled", Disabled);
+	}
+}
