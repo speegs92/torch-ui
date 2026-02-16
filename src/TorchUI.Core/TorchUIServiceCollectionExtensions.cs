cb7f298b5fff6f09f772dae30380481854d87b0f
speegs92 <christian@levesque.dev>
2026-02-16 14:38:36 -0600

    added IServiceCollection.AddTorchUI() extension method for adding TorchUI services to the service collection

---
+++ src/TorchUI.Core/TorchUIServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using TorchUI.Validation;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Contains TorchUI extension methods for <see langword="IServiceCollection"/>
+/// </summary>
+public static class TorchUIServiceCollectionExtensions
+{
+	public static IServiceCollection AddTorchUI(this IServiceCollection self)
+	{
+		self
+			.AddScoped<IValidationAttributeGenerator, DefaultValidationAttributeGenerator>();
+
+		return self;
+	}
+}
