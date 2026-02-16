a66a55bcd0da43d231a631e990a65aebe5f03953
speegs92 <christian@levesque.dev>
2026-02-16 14:37:34 -0600

    added IValidationAttributeGenerator interface and default implementation for creating a dictionary of HTML5 data-* attributes

---
+++ src/TorchUI.Core/Validation/DefaultValidationAttributeGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Reflection;
+using TorchUI.Validation.Mappers;
+
+namespace TorchUI.Validation;
+
+/// <summary>
+/// A default implementation of <see cref="IValidationAttributeGenerator"/>
+/// </summary>
+public class DefaultValidationAttributeGenerator : IValidationAttributeGenerator
+{
+	private readonly IServiceProvider _sp;
+
+	/// <summary>
+	/// Creates a new instance of <c>DefaultValidationAttributeGenerator</c>
+	/// </summary>
+	/// <param name="sp">The service provider for the current scope</param>
+	public DefaultValidationAttributeGenerator(
+		IServiceProvider sp)
+	{
+		_sp = sp;
+	}
+
+	/// <inheritdoc />
+	public Dictionary<string, object> Generate<TValue>(Expression<Func<TValue>> valueExpression)
+	{
+		var htmlAttributes = new Dictionary<string, object>();
+
+		var propertyInfo = GetPropertyInfo(valueExpression);
+
+		if (propertyInfo is null)
+		{
+			return htmlAttributes;
+		}
+
+		var hasValidations = false;
+		var validationAttributes = propertyInfo.GetCustomAttributes<ValidationAttribute>(true);
+
+		foreach (var a in validationAttributes)
+		{
+			var mapper = (IValidationRuleMapper?) _sp.GetService(typeof(IValidationRuleMapper<>).MakeGenericType(a.GetType()));
+
+			if (mapper is not null)
+			{
+				mapper.MapToHtmlAttributes(a, htmlAttributes);
+				hasValidations = true;
+			}
+		}
+
+		if (hasValidations)
+		{
+			htmlAttributes["data-val"] = "true";
+		}
+
+		return htmlAttributes;
+	}
+
+	private static PropertyInfo? GetPropertyInfo<TValue>(Expression<Func<TValue>> valueExpression)
+	{
+		if (valueExpression.Body is MemberExpression memberExpression)
+		{
+			return memberExpression.Member as PropertyInfo;
+		}
+
+		if (valueExpression.Body is UnaryExpression { Operand: MemberExpression unaryMemberExpression })
+		{
+			return unaryMemberExpression.Member as PropertyInfo;
+		}
+
+		return null;
+	}
+
+	
+}
---
+++ src/TorchUI.Core/Validation/IValidationAttributeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TorchUI.Validation;
+
+/// <summary>
+/// An interface for generating HTML attributes to translate .NET validation rules into <c>data-*</c> attributes for use by JavaScript on the client
+/// </summary>
+public interface IValidationAttributeGenerator
+{
+	/// <summary>
+	/// Generates attributes for the given expression 
+	/// </summary>
+	/// <param name="valueExpression">An expression representing the model member for which to generate validation attributes</param>
+	/// <typeparam name="TValue">The type of the model member for which to generate validation attributes</typeparam>
+	/// <returns>A dictionary of validation attributes</returns>
+	Dictionary<string, object> Generate<TValue>(Expression<Func<TValue>> valueExpression);
+}
