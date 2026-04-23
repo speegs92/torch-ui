0189b908c17c272a7d72ca73e48aa341aa9729da
speegs92 <christian@levesque.dev>
2026-04-23 17:07:44 -0500

    added Bootstrap implementation of form validation messaging

---
+++ src/TorchUI.Bootstrap/Components/Validations/BootstrapValidationMessageRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components.Rendering;
+using TorchUI.Validation;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Renders simple error messages in a Bootstrap-aligned manner
+/// </summary>
+public class BootstrapValidationMessageRenderer : IValidationMessageRenderer
+{
+	/// <inheritdoc />
+	public void Render(
+		RenderTreeBuilder builder,
+		IEnumerable<string> validationErrors)
+	{
+		builder.OpenComponent<FormValidationErrorMessage>(0);
+		builder.AddComponentParameter(1, nameof(FormValidationErrorMessage.Errors), validationErrors);
+		builder.CloseComponent();
+	}
+}
---
+++ src/TorchUI.Bootstrap/Components/Validations/FormValidationErrorMessage.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components; 
+
+// Resharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Renders errors for a form field
+/// </summary>
+public partial class FormValidationErrorMessage
+{
+	/// <summary>
+	/// The errors for the current form field
+	/// </summary>
+	[Parameter]
+	public IEnumerable<string> Errors { get; set; } = [];
+
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		CssBuilder.AddClass("list-unstyled text-danger");
+	}
+}
+
