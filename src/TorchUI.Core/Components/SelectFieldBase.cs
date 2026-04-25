41410ef060d6fa8bb48108f47f5bd4454dc93947
speegs92 <christian@levesque.dev>
2026-04-25 12:26:09 -0500

    added base select field to core

---
+++ src/TorchUI.Core/Components/SelectFieldBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using TorchUI.Validation;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI;
+
+/// <summary>
+/// Adds HTML5 <c>data-val-*</c> attributes to selects for client-side validation
+/// </summary>
+/// <typeparam name="TValue">The type of the value</typeparam>
+public class SelectFieldBase<TValue> : Microsoft.AspNetCore.Components.Forms.InputSelect<TValue>
+{
+	[Inject]
+	private IValidationAttributeGenerator ValidationAttributeGenerator { get; set; } = null!;
+
+	/// <inheritdoc />
+	protected override void OnParametersSet()
+	{
+		var attr = ValidationAttributeGenerator.Generate(ValueExpression!);
+
+		if (AdditionalAttributes is not null)
+		{
+			foreach (var kvp in AdditionalAttributes)
+			{
+				attr.Add(kvp.Key, kvp.Value);
+			}
+		}
+
+		AdditionalAttributes = attr;
+	}
+}
