46c9fb2d75a0b17db84176480a2575c53decfebc
speegs92 <christian@levesque.dev>
2026-04-25 14:58:17 -0500

    added <RadioFieldGroup> and <RadioField> components

---
+++ src/TorchUI.Bootstrap/Components/Forms/RadioField.razor.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap radio button
+/// </summary>
+/// <typeparam name="TValue">The type of the value</typeparam>
+public partial class RadioField<TValue>
+{
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		CssBuilder.AddClass("form-check");
+	}
+}
+
