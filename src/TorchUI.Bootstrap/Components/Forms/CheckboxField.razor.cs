a56962efc86d911cc6467079bc4064fdde2c3d2c
speegs92 <christian@levesque.dev>
2026-04-26 15:47:01 -0500

    added checkbox inputs which work with checkbox groups

---
+++ src/TorchUI.Bootstrap/Components/Forms/CheckboxField.razor.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap checkbox
+/// </summary>
+/// <typeparam name="TValue">The type of the value</typeparam>
+public partial class CheckboxField<TValue>
+{
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		CssBuilder.AddClass("form-check");
+	}
+}
+
