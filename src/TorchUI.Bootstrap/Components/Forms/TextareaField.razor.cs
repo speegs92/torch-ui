7a5e325f713bc33f875b34e558168ad1191ae0d6
speegs92 <christian@levesque.dev>
2026-04-26 11:46:49 -0500

    added <TextareaField> component

---
+++ src/TorchUI.Bootstrap/Components/Forms/TextareaField.razor.cs
@@ -0,0 +1,7 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap textarea field
+/// </summary>
+public partial class TextareaField;
