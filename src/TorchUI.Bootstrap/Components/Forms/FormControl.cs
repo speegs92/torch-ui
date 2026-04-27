699686a1129608af7c3132a83c4955087fef44a8
speegs92 <christian@levesque.dev>
2026-04-27 18:07:45 -0500

    added base FormControl class and form control size parameter

---
+++ src/TorchUI.Bootstrap/Components/Forms/FormControl.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using TorchUI.Bootstrap.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.form-control</c>
+/// </summary>
+/// <typeparam name="TValue"></typeparam>
+public abstract class FormControl<TValue> : BaseField<TValue>
+{
+	protected string? FormControlClasses =>
+		new CssBuilder()
+			.AddClass("form-control")
+			.AddClass(Size.GetSizeClass("form-control"), Size is not Size.Medium)
+			.Build();
+
+	/// <summary>
+	/// The form control's size
+	/// </summary>
+	[Parameter]
+	public Size Size { get; set; } = Size.Medium;
+}
