b75abbb601b6a644cd2042f651f5ec7f0923afdb
speegs92 <christian@levesque.dev>
2026-04-29 19:04:53 -0500

    added <StandaloneCheckboxField> component

---
+++ src/TorchUI.Bootstrap/Components/Forms/StandaloneCheckboxField.razor.cs
@@ -0,0 +1,7 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap checkbox which stores a standalone, boolean value
+/// </summary>
+public partial class StandaloneCheckboxField;
