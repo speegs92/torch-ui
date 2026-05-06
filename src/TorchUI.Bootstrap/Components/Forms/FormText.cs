9ecb05ef3473ef754a8bdbe18e29ae18d1ab8046
speegs92 <christian@levesque.dev>
2026-05-06 17:28:21 -0500

    added <FormText> component

---
+++ src/TorchUI.Bootstrap/Components/Forms/FormText.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable once CheckNamespace
+namespace TorchUI.Bootstrap.Components;
+
+/// <summary>
+/// Represents a Bootstrap <c>.form-text</c>
+/// </summary>
+public class FormText : TorchComponentBase
+{
+	/// <inheritdoc />
+	protected override void SetupAttributes()
+	{
+		CssBuilder.AddClass("form-text");
+	}
+}
